b4fb10573d2daa08c9e9ffdbfbe8e11099fd4b24
liuhll <1029765111@qq.com>
2020-12-15 23:02:31 +0800

    优化用户组模块

--- src/views/authorization/user-group/components/UserGroupForm.vue
+++ src/views/authorization/user-group/components/UserGroupForm.vue
@@ -8,30 +8,50 @@
       :rules="rules"
       :disabled="dialogStatus === 'look'"
     >
-      <el-form-item label="用户组名" prop="name" >
+      <el-form-item label="用户组名" prop="name">
         <el-input v-model="userGroup.name" placeholder="请输入用户组名" />
       </el-form-item>
-      <el-form-item prop="roleIds" label="角色" >
-            <el-tooltip class="item" effect="dark" content="请输入关键字查找要添加的角色" placement="top-start">
-                <el-select
-                  v-model="userGroup.roleIds"
-                  multiple
-                  clearable
-                  filterable
-                  remote
-                  :loading="loading"
-                  :remote-method="searchRoles"
-                  style="width: 100%"
-                  placeholder="请输入要添加的角色名称"
-                >
-                  <el-option
-                    v-for="item in roles"
-                    :key="item.id"
-                    :label="item.name"
-                    :value="item.id"
-                  ></el-option>
-                </el-select>
-            </el-tooltip>
+      <el-form-item prop="roleIds" label="角色">
+        <el-tooltip
+          class="item"
+          effect="dark"
+          content="请输入关键字查找要添加的角色"
+          placement="top-start"
+        >
+          <el-select
+            v-model="userGroup.roleIds"
+            multiple
+            clearable
+            filterable
+            remote
+            :loading="loading"
+            :remote-method="searchRoles"
+            style="width: 100%"
+            placeholder="请输入要添加的角色名称"
+          >
+            <el-option
+              v-for="item in roles"
+              :key="item.id"
+              :label="item.name"
+              :value="item.id"
+            ></el-option>
+          </el-select>
+        </el-tooltip>
+      </el-form-item>
+      <el-form-item label="权限" prop="permissionIds">
+        <el-scrollbar style="height: 300px">
+          <el-tree
+            ref="permissionTree"
+            :data="permissionData"
+            show-checkbox
+            node-key="permissionId"
+            :default-expand-all="true"
+            :default-checked-keys="userGroup.permissionIds"
+            :props="permissionProps"
+            @check-change="handleCheckPermission"
+          >
+          </el-tree>
+        </el-scrollbar>
       </el-form-item>
       <el-form-item label="备注" prop="memo">
         <el-input type="textarea" v-model="userGroup.memo" />
@@ -64,25 +84,39 @@ export default {
       },
       loading: false,
       roles: [],
+      permissionData: [],
+      permissionProps: {
+        children: "children",
+        label: "title",
+      },
       queryRole: {
         searchKey: undefined,
         status: 1,
         pageCount: 10,
         pageIndex: 1,
-      },       
+      },
     };
+  },
+  mounted() {
+    this.loadPermissionData();
   },
   methods: {
     ...mapActions("role", ["list", "search"]),
+    ...mapActions("menu", ["getTree"]),
     loadRoleData() {
       this.list().then((data) => {
         this.roles = data;
-      })
+      });
+    },
+    loadPermissionData() {
+      this.getTree().then((data) => {
+        this.permissionData = data;
+      });
     },
     searchRoles(key) {
-      if (key !== '' || !this.userGroup.roleIds) {
-      this.queryRole.searchKey = key;
-      this.loadRoleData()
+      if (key !== "" || !this.userGroup.roleIds) {
+        this.queryRole.searchKey = key;
+        this.loadRoleData();
       }
     },
     loadRoleData() {
@@ -95,8 +129,11 @@ export default {
           this.loading = false;
         }, 1.5 * 200);
       });
-    },    
-  }
+    },
+    handleCheckPermission(node, checked, indeterminate) {
+      this.userGroup.permissionIds = this.$refs["permissionTree"].getCheckedKeys();
+    }
+  },
 };
 </script>
 
